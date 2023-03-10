7d208b7334c49df93762ee3d957b2b5c1286154a
Octavio Fabris <110264131+octifabris@users.noreply.github.com>
2023-03-10 11:00:00 -0300

    Add files via upload

---
+++ Ejercicios/practica-5ta-session/ejercicio5.js
@@ -0,0 +1,8 @@
+let altura_cm = 175;
+let altura_mts = 1.75;
+let peso_kg = 78.5;
+let alt_mts_round_hgh = Math.ceil(altura_mts);
+console.log(alt_mts_round_hgh);
+let peso_kg_round_low = Math.floor(peso_kg);
+console.log(peso_kg_round_low);
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
